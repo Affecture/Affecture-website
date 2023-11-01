0d78f0ca1a3389cb9d63486da4871bd7119cf12b
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2023-11-01 08:03:51 +0100

    add astro-plugin to prettier

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,1 +1,13 @@
-export default {};
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+    plugins: ['prettier-plugin-astro'],
+    overrides: [
+      {
+        files: '*.astro',
+        options: {
+          parser: 'astro',
+        },
+      },
+    ],
+  };
