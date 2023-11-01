9b92d1a7d869cd5181d819ddc5d813e64faa2788
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2023-11-01 20:20:16 +0100

    fix formatting

--- src/config.mjs
+++ src/config.mjs
@@ -7,7 +7,9 @@ export default {
     description:
       "games user research using biosignals and AI to deliver deeper and faster results",
   },
-  url: process.env.CI ? "https://affecture.github.io/Affecture-website/" : "http://localhost:4321",
+  url: process.env.CI
+    ? "https://affecture.github.io/Affecture-website/"
+    : "http://localhost:4321",
   social: {
     github: false,
     linkedin: "https://www.linkedin.com/company/affecture/",
