e53d2d446ef21c8431f6ea8f694b85a41f6303b3
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2023-11-01 19:20:27 +0100

    use localhost when not in CI

--- src/config.mjs
+++ src/config.mjs
@@ -7,7 +7,7 @@ export default {
     description:
       "games user research using biosignals and AI to deliver deeper and faster results",
   },
-  url: "https://some-domain.com",
+  url: process.env.CI ? "https://some-domain.com": "http://localhost:4321",
   social: {
     github: false,
     linkedin: "https://www.linkedin.com/company/affecture/",
