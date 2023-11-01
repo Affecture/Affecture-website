440f0ff4c089312d0674a77d6be43aba4741f356
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2023-11-01 17:13:39 +0100

    add brand colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#010d26",
+        purple: "#8508fb",
+        pink: "#d026e1",
+        complementary: "#f3d3a0"
+      }
+    },
   },
   plugins: [],
 };
