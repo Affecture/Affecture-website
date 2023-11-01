dc8a0d03ca047f83579386825522f5adda6f55db
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2023-11-01 20:18:21 +0100

    fix public URL

--- src/config.mjs
+++ src/config.mjs
@@ -7,7 +7,7 @@ export default {
     description:
       "games user research using biosignals and AI to deliver deeper and faster results",
   },
-  url: process.env.CI ? "https://some-domain.com" : "http://localhost:4321",
+  url: process.env.CI ? "https://affecture.github.io/Affecture-website/" : "http://localhost:4321",
   social: {
     github: false,
     linkedin: "https://www.linkedin.com/company/affecture/",
