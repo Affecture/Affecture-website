efcd0c44a3d3681f642ea4e855a0c6155a0135a7
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2023-11-01 18:10:13 +0100

    enable dynamic imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 import { url } from "./src/config.mjs";
+
+import dynamicImport from "vite-plugin-dynamic-import";
+
 import mdx from "@astrojs/mdx";
 
 import tailwind from "@astrojs/tailwind";
@@ -7,5 +10,8 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: url,
+  vite: {
+    plugins: [dynamicImport()],
+  },
   integrations: [mdx(), tailwind()],
 });
