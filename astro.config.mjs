307261ab6a5cb062c33a75ede719c187b231a6e0
Castelli0 <100427083+Castelli0@users.noreply.github.com>
2024-01-25 19:54:00 +0100

    Setup email.js and update contact form feedback(#172)
    Co-authored-by: Hector Castelli Zacharias <hector.zacharias@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from "astro/config";
 import config from "./src/config.mjs";
-
 const { url } = config;
-
 import dynamicImport from "vite-plugin-dynamic-import";
-
 import mdx from "@astrojs/mdx";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
