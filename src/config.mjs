21be27a080f8ed27bbc8a6fa57acbb0d8eaed3c7
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2024-01-25 18:29:28 +0100

    remove base path

--- src/config.mjs
+++ src/config.mjs
@@ -8,7 +8,6 @@ export default {
       "games user research using biosignals and AI to deliver deeper and faster results",
   },
   url: import.meta.env.PROD ? "https://affecture.com" : "http://localhost:4321",
-  base: import.meta.env.PROD ? "/Affecture-website" : undefined,
   social: {
     github: false,
     linkedin: "https://www.linkedin.com/company/affecture/",
